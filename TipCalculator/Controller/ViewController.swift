de483a83065bf08084f0f1f94267bd1bb8a113bb
Emelyanov Roman <wohagy@gmail.com>
2022-01-28 16:31:35 +0300

    first commit

---
+++ TipCalculator/Controller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TipCalculator
+//
+//  Created by Macbook on 28.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
