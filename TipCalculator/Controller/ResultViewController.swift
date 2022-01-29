8590fb38a655eac9b2a3b62ce7b3a5fc6b2f7564
Полина Шубина <polinasubina@MacBook-Pro.local>
2022-01-29 13:21:47 +0300

    added segue and VC for result screen

---
+++ TipCalculator/Controller/ResultViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ResultViewController.swift
+//  TipCalculator
+//
+//  Created by Полина Шубина on 29.01.2022.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+    var someProperty = 0
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- TipCalculator/Controller/ViewController.swift
+++ TipCalculator/Controller/ViewController.swift
@@ -8,12 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBAction func calculateButtonAction(_ sender: UIButton) {
+        performSegue(withIdentifier: "goToResult", sender: self)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "goToResult" {
+            let resultVC = segue.destination as! ResultViewController
+            print(resultVC.someProperty)
+        }
+    }
 
 }
 
